7945a5f3b42dd72536a1b5c3f790ebaa871a6293
Lypzis <victor.piccoli@pm.me>
2024-08-02 08:59:50 -0300

    update

--- 02-api-rest/src/env/index.ts
+++ 02-api-rest/src/env/index.ts
@@ -6,8 +6,9 @@ process.env.NODE_ENV === 'test' ? config({ path: '.env.test' }) : config();
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 });
 
 // validation that dburl exists and is a string
